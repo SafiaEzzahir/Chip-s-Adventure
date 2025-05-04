58559dc398342ff83199b6e8685a1943c53f28db
SafiaEzzahir <safia.ezzahir@hotmail.com>
2025-05-04 17:56:37 +0100

    grid layout

--- the lab/main.py
+++ the lab/main.py
@@ -1,4 +1,5 @@
 from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
