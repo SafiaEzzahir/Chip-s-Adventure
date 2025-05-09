7ad9dc017ac54140bdc54050d1e91ca729a6a826
SafiaEzzahir <safia.ezzahir@hotmail.com>
2025-05-09 22:48:55 +0100

    straight line at the start

--- practice/galaxy/main.py
+++ practice/galaxy/main.py
@@ -44,6 +44,7 @@ class MainWidget(Widget):
         self.init_vertical_lines()
         self.init_horizontal_lines()
         self.init_tiles()
+        self.pre_fill_tiles_coordinates()
         self.generate_tiles_coordinates()
         if self.is_desktop():
             self._keyboard = Window.request_keyboard(self.keyboard_closed, self)
@@ -61,6 +62,10 @@ class MainWidget(Widget):
             Color(1, 1, 1)
             for i in range(0, self.NB_TILES):
                 self.tiles.append(Quad())
+
+    def pre_fill_tiles_coordinates(self):
+        for i in range(0, 10):
+            self.tiles_coordinates.append((0, i))
 
     def generate_tiles_coordinates(self):
         last_y = 0
