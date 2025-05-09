2733ef9bf7b323e6cb200493ea26ba330bc6137f
SafiaEzzahir <safia.ezzahir@hotmail.com>
2025-05-09 16:48:27 +0100

    moving to the side

--- practice/galaxy/main.py
+++ practice/galaxy/main.py
@@ -1,3 +1,7 @@
+from kivy.config import Config
+Config.set("graphics", "width", "900")
+Config.set("graphics", "height", "400")
+
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, Clock
@@ -18,6 +22,9 @@ class MainWidget(Widget):
 
     SPEED = 4
     current_offset_y = 0
+
+    SPEED_X = 4
+    current_offset_x = 0
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -58,7 +65,7 @@ class MainWidget(Widget):
         spacing = self.V_LINES_SPACING*self.width
         offset = -int(self.V_NB_LINES/2)+0.5
         for i in range(0, self.V_NB_LINES):
-            line_x = int(central_line_x + offset*spacing)
+            line_x = int(central_line_x + offset*spacing + self.current_offset_x)
             x1, y1 = self.transform(line_x, 0)
             x2, y2 = self.transform(line_x, self.height)
             self.vertical_lines[i].points = [x1, y1, x2, y2]
@@ -74,8 +81,8 @@ class MainWidget(Widget):
         central_line_x = int(self.width/2)
         spacing = self.V_LINES_SPACING*self.width
         offset = int(self.V_NB_LINES/2)-0.5
-        xmin = central_line_x - offset * spacing
-        xmax = central_line_x + offset * spacing
+        xmin = central_line_x - offset * spacing + self.current_offset_x
+        xmax = central_line_x + offset * spacing + self.current_offset_x
         spacing_y = self.H_LINES_SPACING*self.height
         for i in range(0, self.H_NB_LINES):
             line_y = i * spacing_y-self.current_offset_y
@@ -109,6 +116,7 @@ class MainWidget(Widget):
         spacing_y = self.H_LINES_SPACING*self.height
         if self.current_offset_y >= spacing_y:
             self.current_offset_y -= spacing_y
+        self.current_offset_x += self.SPEED_X*time_factor
 
 class GalaxyApp(App):
     pass
