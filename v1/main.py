35df93863fe15f378ecc16bf2b5fe60e1acd738c
SafiaEzzahir <safia.ezzahir@hotmail.com>
2025-05-10 14:33:14 +0100

    background updates automatically

--- v1/main.py
+++ v1/main.py
@@ -1,10 +1,35 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.graphics.vertex_instructions import Rectangle
+from kivy.properties import Clock
+from kivy.graphics.context_instructions import Color
 
 class MainWidget(Widget):
-    pass
 
-class version1app(App):
-    pass
+    baaground = None
+    mb_colr, mb_colg, mb_colb = 1, 0.7922, 0.686275
 
-version1app().run()
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        print("baa")
+        self.init_background()
+        Clock.schedule_interval(self.update, 1.0/60.0)
+
+    def init_background(self):
+        with self.canvas:
+            Color(self.mb_colr, self.mb_colg, self.mb_colb)
+            self.baaground = Rectangle(size=self.size)
+
+    def update_background(self):
+        with self.canvas:
+            Color(self.mb_colr, self.mb_colg, self.mb_colb)
+            self.baaground = Rectangle(size=self.size)
+
+    def update(self, dt):
+        self.update_background()
+
+class Version1app(App):
+    def build(self):
+        return MainWidget()
+
+Version1app().run()
