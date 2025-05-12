0c1a9be9ad00cd382664b827044050ed1470d937
SafiaEzzahir <safia.ezzahir@hotmail.com>
2025-05-12 17:41:29 +0100

    generate play button function

--- v3/screens/menu.py
+++ v3/screens/menu.py
@@ -4,15 +4,13 @@ from kivy.graphics.context_instructions import Color
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.metrics import dp
+from kivy.utils import get_color_from_hex
 
 class MenuScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.name = "menu"
         self.backround = None
-        self.mb_colr = 1
-        self.mb_colg = 0.7922
-        self.mb_colb = 0.686275
         self.lbl = dp(80)
         self.font = "assets/fontly.ttf"
         self.fontsize = self.lbl/2
@@ -20,14 +18,13 @@ class MenuScreen(Screen):
     def onplay(self, instance):
         pass
 
+    def generateplaybutton(self, size, pos):
+        return Button(text="play", size_hint=(None, None), size=size, pos=pos, on_press=self.onplay, font_name=self.font, font_size=self.fontsize)
+
     def update(self):
         self.lblx = self.center_x-self.lbl/2
         self.lbly = self.center_y-self.lbl/2
-        with self.canvas:
-            Color(self.mb_colr, self.mb_colg, self.mb_colb)
-            self.backround = Rectangle(size=self.size)
-        self.playbutton = Button(text="play", size_hint=(None, None), size=(self.lbl, self.lbl-dp(20)), pos=(self.lblx, self.lbly), on_press=self.onplay, font_name=self.font, font_size=self.fontsize)
+        self.playbutton = self.generateplaybutton((self.lbl, self.lbl-dp(20)), (self.lblx, self.lbly))
         self.playbutton.background_normal = ""
         self.playbutton.background_down = ""
-        self.playbutton.background_color = (1, 1, 0, 1)
         self.add_widget(self.playbutton)
