d5da58757849e8f16ebcdd95ecd351ca183fff72
SafiaEzzahir <safia.ezzahir@hotmail.com>
2025-05-26 11:11:17 +0100

    settings screen + volume slider

--- v3/screens/settings.py
+++ v3/screens/settings.py
@@ -6,6 +6,7 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.metrics import dp
 from kivy.core.audio import SoundLoader
+from kivy.uix.slider import Slider
 
 class SettingsScreen(Screen):
     def __init__(self, **kw):
@@ -21,10 +22,13 @@ class SettingsScreen(Screen):
         self.home_button_sizes = (dp(413/8), dp(500/8))
         self.home_button_padding = dp(10)
 
-
         with self.canvas:
             Color(*self.mediumbrown)
             self.backround = Rectangle(size=self.size)
+
+        self.volume_slider = Slider(min=0, max=100, value=100, pos=(0, 0), size_hint=(None, None), size=(self.width/3, dp(10)), background_horizontal="assets/brownslider.png", background_width=dp(5), cursor_image="assets/browncursor.png")
+        self.add_widget(self.volume_slider)
+        self.widgets = [self.volume_slider]
 
         self.labels = [self.init_label("volume", (0,0), self.mediumcream), self.init_label("darkmode", (0,0), self.mediumcream), self.init_label("life", (0,0), self.mediumcream)]
 
@@ -51,8 +55,14 @@ class SettingsScreen(Screen):
     def update_label_pos(self, label, num):
         interval = (self.title.pos[1]-self.labels_height)/len(self.labels)
         y = self.title.pos[1]-interval*num
-        x = self.center_x-label.size[0]/2
+        x = self.width/6-label.size[0]/2
         label.pos = (x, y)
+
+    def update_widget_pos(self, widget, num):
+        y = self.labels[num].pos[1]+widget.size[1]
+        x = self.width/6*3-widget.size[0]/2
+        widget.width = self.width/3
+        widget.pos = (x, y)
         
     def init_label(self, text, pos, col):
         label = Label(text=text, pos=pos, color=col, font_size=dp(30), font_name="v3/assets/fontly.ttf", size_hint=(None, None), size=(dp(200), dp(40)))
@@ -66,6 +76,10 @@ class SettingsScreen(Screen):
         for labelnum in range(len(self.labels)):
             label = self.labels[labelnum]
             self.update_label_pos(label, labelnum+1)
+
+        for widgetnum in range(len(self.widgets)):
+            widget = self.widgets[widgetnum]
+            self.update_widget_pos(widget, widgetnum)
 
         self.title.center_x = self.center_x
         self.title.center_y = self.height - dp(50)
